073e77d56a5305da5fd39b80bb337e23f78df580
Ivan Arabadzhiyski <ivan.arabadzhiyski@advance-auto.com>
2022-12-05 15:11:38 +0200

    day5

--- solutions/day5/main.py
+++ solutions/day5/main.py
@@ -10,6 +10,18 @@ def transpose(matrix: List[List]):
              for j in range(len(matrix))] for i in range(len(matrix[0]))]
 
 
+def move_boxes(stacks, commands, cratemover9001: bool):
+    stacks = deepcopy(stacks)
+    for command in commands:
+        amount, start, end = command
+        selected = stacks[start-1][-amount:]
+        if not cratemover9001:
+            selected = selected[::-1]
+        stacks[end-1] += selected
+        del stacks[start-1][-amount:]
+    return ''.join([x[-1] for x in stacks])
+
+
 def solve():
     stacks_input, commands = stdin.read().split('\n\n')
     stacks_input = stacks_input.splitlines(keepends=True)[:-1]
@@ -22,18 +34,12 @@ def solve():
     # filter for empty string
     stacks = [[y for y in x if y != ' '] for x in stacks]
 
-    stacks_part2 = deepcopy(stacks)
+    commands = [list(map(int, re.split(r"\W+", command)[1::2]))
+                for command in commands.splitlines()]
 
-    for command in commands.splitlines():
-        split_command = re.split(r"\W+", command)
-        move, _from, to = list(map(int, split_command[1::2]))
-        for i in range(move):
-            stacks[to - 1].append(stacks[_from - 1].pop())
-            stacks_part2[to - 1].append(stacks_part2[_from - 1][-(move-i)])
-        stacks_part2[_from - 1] = stacks_part2[_from - 1][:-move]
-
-    return (''.join([x[-1] for x in stacks]), ''.join([x[-1] for x in stacks_part2]))
+    print(move_boxes(stacks, commands, cratemover9001=False))
+    print(move_boxes(stacks, commands, cratemover9001=True))
 
 
 if __name__ == '__main__':
-    print(*solve())
+    solve()
