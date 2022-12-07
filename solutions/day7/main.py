822bd64bc3b7d3be127e2f44db3240c4081ee584
Ivan Arabadzhiyski <ivan.arabadzhiyski@advance-auto.com>
2022-12-07 10:49:09 +0200

    day7

--- solutions/day7/main.py
+++ solutions/day7/main.py
@@ -6,54 +6,54 @@ from typing import List, Optional,  Union
 
 
 class File():
-    def __init__(self, name: str, size: int):
-        self.name = name
-        self.size = size
+    def __init__(self, name: str, size: Optional[int] = None):
+        self._name = name
+        self._size = size
 
     def to_string(self, index: int):
-        return " " * (index * 2) + f"- {self.name} (file, size={self.size})"
+        return " " * (index * 2) + f"- {self._name} (file, size={self._size})"
 
-    def get_total_size(self):
-        return self.size
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def name(self):
+        return self._name
 
 
-class Directory():
-    def __init__(self, name: str = '', parent=None):
-        self.name = name
-        self.files: List[Union[File, Directory]] = []
-        self.parent = parent
-        self.total_size: Optional[int] = None
+class Directory(File):
+    def __init__(self, name: str = ''):
+        super().__init__(name)
+        self.files: List[File] = []
 
-    def add_file(self, file: File):
+    def add(self, file: File):
         self.files.append(file)
 
-    def add_dir(self, dir):
-        self.files.append(dir)
-        dir.parent = self
-
-    def get_child(self, name: str):
+    def get_subdir(self, name: str):
         for f in self.files:
             if f.name == name and isinstance(f, Directory):
                 return f
         raise ValueError
 
-    def get_total_size(self):
-        if self.total_size is None:
-            self.total_size = reduce(
-                lambda curr, next: curr + next.get_total_size(), self.files, 0)
-        return self.total_size
+    @property
+    def size(self):
+        if self._size is None:
+            self._size = reduce(
+                lambda curr, next: curr + next.size, self.files, 0)
+        return self._size
 
     def to_string(self, index: int = 0):
         ret = " " * (index * 2) + \
-            f"- {self.name} (dir) - {self.get_total_size()}\n"
+            f"- {self._name} (dir)\n"
         for f in self.files:
             ret += f.to_string(index+1) + '\n'
         return ret
 
-    def get_all_dirs_with_size_compared_to_threshold(self, threshold: int, comparator=lambda x, y: x < y) -> List[int]:
+    def get_all_dirs_with_size_compared_to_threshold(self, threshold: int, comparator=lambda x, y: x < y):
         result: List[int] = []
-        if comparator(self.get_total_size(), threshold):
-            result.append(self.get_total_size())
+        if comparator(self.size, threshold):
+            result.append(self.size)
         for f in self.files:
             if isinstance(f, Directory):
                 result += f.get_all_dirs_with_size_compared_to_threshold(
@@ -62,32 +62,32 @@ class Directory():
 
 
 def solve() -> None:
-    root = Directory('/')
+    dir_hierarchy = [Directory('/')]
     input = stdin.read().splitlines()
     for line in input[1:]:
+        node = dir_hierarchy[-1]
         if line == '$ ls':
             continue
         if line.startswith('$ cd '):
             dir_name = line.split('$ cd ')[1]
             if dir_name == '..':
-                root = root.parent
+                dir_hierarchy.pop()
                 continue
-            root = root.get_child(dir_name)
+            dir_hierarchy.append(node.get_subdir(dir_name))
             continue
         if line.startswith('dir '):
-            root.add_dir(Directory(line.split('dir ')[1]))
+            node.add(Directory(line.split('dir ')[1]))
             continue
         size, name = line.split(' ')
-        root.add_file(File(name, int(size)))
+        node.add(File(name, int(size)))
 
-    while root is not None and root.parent is not None:
-        root = root.parent
-
+    root = dir_hierarchy[0]
     print(root.to_string())
     print(sum(root.get_all_dirs_with_size_compared_to_threshold(100000)))
+
     update_size = 30000000
     max_size = 70000000
-    space_needed = update_size - (max_size-root.get_total_size())
+    space_needed = update_size - (max_size-root.size)
     print(min(root.get_all_dirs_with_size_compared_to_threshold(
         space_needed, lambda x, y: x > y)))
 
