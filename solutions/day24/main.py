fb44486bb2d412d938caaefbf2a3a32b93208e2d
Ivan Arabadzhiyski <ivan.arabadzhiyski@advance-auto.com>
2022-12-24 16:10:03 +0200

    day24

--- solutions/day24/main.py
+++ solutions/day24/main.py
@@ -5,26 +5,41 @@ from typing import Dict, List, Set
 
 
 def lcm(a, b):
-    return (a / math.gcd(a, b)) * b
+    return int(a * b / math.gcd(a, b))
 
 
 direction_to_coords = {'<': (0, -1), '>': (0, 1), 'v': (1, 0), '^': (-1, 0)}
 
 
+def bfs(start, end, blizzards, blizzards_repeat_after, pathway, starting_time=1):
+    h, l = len(pathway), len(pathway[0])
+    q = [(start, starting_time)]
+
+    visited = {(start, starting_time)}
+    while q:
+        pos, time = q.pop(0)
+        x, y = pos
+        b = blizzards[time % blizzards_repeat_after]
+        for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0), (0, 0)):
+            nx, ny = x+dx, y+dy
+            if (nx, ny) == end:
+                return time
+            if 0 < nx < h-1 and 0 < ny < l-1 and (nx, ny, '>') not in b and (nx, ny, '<') not in b and (nx, ny, 'v') not in b and (nx, ny, '^') not in b or (nx, ny) == (0, 1):
+                if ((nx, ny), time+1) not in visited:
+                    q.append(((nx, ny), time+1))
+                    visited.add(((nx, ny), time+1))
+
+
 def solve():
     pathway = [list(x) for x in open(0).read().strip().splitlines()]
     h, l = len(pathway), len(pathway[0])
-    print(h, l)
     size_x, size_y = h-2, l-2
-    start_x, start_y, goal_x, goal_y = 0, pathway[0].index(
-        '.'), h-1, pathway[-1].index('.')
-    blizzards_repeat_after = int(lcm(size_x, size_y))
+    start, goal = (0, pathway[0].index(
+        '.')), (h-1, pathway[-1].index('.'))
+    blizzards_repeat_after = lcm(size_x, size_y)
     blizzards: List[Set] = []
-    d = set()
-    for i in range(1, len(pathway)):
-        for j in range(1, len(pathway[0])):
-            if pathway[i][j] in ['<', '>', 'v', '^']:
-                d.add((i, j, pathway[i][j]))
+    d = {(i, j, pathway[i][j]) for i in range(1, h)
+         for j in range(1, l) if pathway[i][j] in '<>v^'}
     blizzards.append(d)
     for _ in range(blizzards_repeat_after-1):
         b = blizzards[-1]
@@ -32,33 +47,18 @@ def solve():
         for x, y, direction in b:
             dx, dy = direction_to_coords[direction]
             # 0->4, 1->1, 2->2,3->3,4->4,5->1
-            nx, ny = (dx+x), (dy+y)
-            if nx > size_x:
-                nx %= size_x
-            if ny > size_y:
-                ny %= size_y
-            if nx == 0:
-                nx = size_x
-            if ny == 0:
-                ny = size_y
+            nx, ny = ((dx+x-1) % size_x)+1, ((dy+y-1) % size_y)+1
             d.add((nx, ny, direction))
         blizzards.append(d)
-    q = [(start_x, start_y, 1)]
-    while q:
-        x, y, time = q.pop(0)
-        # print(x, y, time)
-        b = blizzards[time % blizzards_repeat_after]
-        added = False
-        for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            nx, ny = x+dx, y+dy
-            if nx == goal_x and ny == goal_y:
-                return time
-            if nx > 0 and nx < h-1 and ny > 0 and ny < l-1 and (nx, ny, '>') not in b and (nx, ny, '<') not in b and (nx, ny, 'v') not in b and (nx, ny, '^') not in b:
-                added = True
-                q.append((nx, ny, time+1))
-        if not added:
-            q.append((x, y, time+1))
-        # print(q)
+
+    first_trip = bfs(start, goal,
+                     blizzards, blizzards_repeat_after, pathway)
+    second_trip = bfs(goal, start,
+                      blizzards, blizzards_repeat_after, pathway, starting_time=first_trip)
+
+    third_trip = bfs(start, goal,
+                     blizzards, blizzards_repeat_after, pathway, starting_time=first_trip+second_trip)
+    return first_trip, sum([first_trip, second_trip, third_trip])
 
 
 if __name__ == '__main__':
